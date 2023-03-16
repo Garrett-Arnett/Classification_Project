d696d19a9db9387e12fbea92e5a23e536eae42d9
“Garrett <garrett.paul.arnett@gmail.com>
2023-03-16 15:10:51 -0500

    acquire file

---
+++ acquire.py
@@ -0,0 +1,16 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Imports
+
+import pandas as pd 
+import csv  #importing the library
+import os
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# For Telco data
+
+def acquire_telco_data(df):
+    df = "telco.csv"
+    df = pd.read_csv(df)
+    df = pd.DataFrame(df)
+    return df
